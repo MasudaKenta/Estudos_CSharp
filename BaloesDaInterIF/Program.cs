3388d4215fa9efa02fbb5cc867b84fd5c9af9c5b
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2023-04-19 21:48:28 -0300

    Add files via upload

---
+++ BaloesDaInterIF/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaloesDaInterIF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] res = Console.ReadLine().Split(' ');
+            int[] balao = new int[n];
+            balao = Array.ConvertAll(res, int.Parse);
+            int[] aux = new int[1000001];
+            int flechas = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (aux[balao[i]] > 0)
+                {
+                    aux[balao[i]]--;
+                    aux[balao[i] - 1]++;
+                }
+                else
+                {
+                    flechas++;
+                    aux[balao[i] - 1]++;
+                }
+            }
+
+            Console.WriteLine(flechas);
+        }
+    }
+}
